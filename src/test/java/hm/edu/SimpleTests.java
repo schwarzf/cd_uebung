ac06445061883926959cb2dd0b58dcdaefa2172a
Florian Schwarz <florian.schwarz2@gmail.com>
2012-10-25 22:38:18 +0200

    added 2 tests

---
+++ src/test/java/hm/edu/SimpleTests.java
@@ -0,0 +1,19 @@
+package hm.edu;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class SimpleTests {
+
+	@Test
+	public void alwaysTrue() {
+		Assert.assertEquals("foo", "foo");
+	}
+	
+	@Test
+	public void anotherTest() {
+		Assert.assertTrue(true);
+	}
+	
+}
